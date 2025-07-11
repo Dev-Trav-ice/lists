1917d17c6dd1b756ba9c08d2b7b8c473511b7326
Dennis Muganda <butunyidennis@gmail.com>
2025-07-11 19:30:53 +0300

    final touches

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,18 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import Navbar from "./components/navbar.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import { BrowserRouter, Route, Routes } from "react-router";
+import App from "./App.tsx";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <Navbar />
-    <App />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </QueryClientProvider>
 );
